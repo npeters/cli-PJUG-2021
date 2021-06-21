9921e90ddc0c2be24966996292646b01abc65dbd
Nicolas Peters <nicolas.peters@canal-plus.com>
2021-06-21 22:06:13 +0200

    github

--- 06_oauth/GithubCli.java
+++ 06_oauth/GithubCli.java
@@ -21,10 +21,40 @@ import java.util.function.Function;
 import static java.lang.System.out;
 import static com.pivovarit.function.ThrowingConsumer.unchecked;
 
+/*
+ *   Device flow implementation
+ *   ==========================
+ * 
+ *   ┌───────┐1
+ *   │       ├───────────────────┐
+ *   │  CLI  │      Device Code  │
+ *   │       ├────────────────┐  │
+ *   └──┬────┘4 Access Token  │  │
+ *      │                     │  │
+ *      │                  ┌──▼──▼────┐
+ * Verification            │          │
+ *    URL                  │  Github  │
+ *      │2                 │          │
+ *      │                  └─────▲────┘
+ *   ┌──▼──────┐                 │
+ *   │         │   User Code     │
+ *   │ Browser ├─────────────────┘
+ *   │         │ 3
+ *   └─────────┘
+ *                              draw with https://asciiflow.com/
+ *
+ *
+ *
+ * The device flow allows you to authorize users for a headless app, such as a
+ * CLI tool or Git credential manager.
+ *
+ * https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps#device-flow
+ *
+ */
+
 @Command(name = "GithubCli", mixinStandardHelpOptions = true, version = "GithubCli 0.1", description = "GithubCli made with jbang")
 class GithubCli implements Callable<Integer> {
 
-    // https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps#rate-limits-for-the-device-flow
     record DeviceCodeResponse(String device_code, String user_code, String verification_uri, long expires_in,
             int interval) {
     }
@@ -42,9 +72,6 @@ class GithubCli implements Callable<Integer> {
 
     // App ID
     String CLIENT_ID = "3084f75f3fb615b86b22";
-
-    @Parameters(index = "0", description = "The greeting to print", defaultValue = "World!")
-    private String greeting;
 
     public static void main(String... args) {
         int exitCode = new CommandLine(new GithubCli()).execute(args);
@@ -66,14 +93,19 @@ class GithubCli implements Callable<Integer> {
         long expiresInstant = ZonedDateTime.now().plusSeconds(deviceCodeResponse.expires_in).toInstant().toEpochMilli();
         boolean hasSuccess = false;
         AccessTokenResponse accessTokenResponse = null;
+        // wait until accessTokenResponse
         while (expiresInstant > System.currentTimeMillis() && !hasSuccess) {
 
             accessTokenResponse = getAccessTokenResponse(client, deviceCodeResponse);
-
             if (accessTokenResponse instanceof ErrorAccessTokenResponse error) {
-                out.printf("%s %s %n", error.error, error.error_description);
-                out.printf("wait...%n");
-                Thread.sleep(deviceCodeResponse.interval * 1000L);
+                if ("authorization_pending".equals(error.error)) {
+                    out.printf("%s %s %n", error.error, error.error_description);
+                    out.printf("wait...%n");
+                    Thread.sleep(deviceCodeResponse.interval * 1000L);
+                } else {
+                    // fatal error
+                    break;
+                }
             } else if (accessTokenResponse instanceof SuccessAccessTokenResponse success) {
                 hasSuccess = true;
             }
@@ -83,10 +115,13 @@ class GithubCli implements Callable<Integer> {
             out.printf("%s %s %n", error.error, error.error_description);
             return -1;
         } else if (accessTokenResponse instanceof SuccessAccessTokenResponse success) {
-            out.printf("access_token: %s %n", success.access_token);
+            String accessTokenPrint = success.access_token.replaceFirst(".{8}$", "****");
+       	    out.printf("access_token: %s %n", accessTokenPrint);
+            return 0;
+        } else {
+            throw new IllegalStateException();
         }
 
-        return 0;
     }
 
     private AccessTokenResponse getAccessTokenResponse(HttpClient client, DeviceCodeResponse deviceCodeResponse)
@@ -105,7 +140,7 @@ class GithubCli implements Callable<Integer> {
                         // out.println(s);
                         var jsonNode = mapper.readTree(s);
                         if (jsonNode.has("error")) {
-                            return mapper.treeToValue(jsonNode, ErrorAccessTokenResponse.class);
+                                return mapper.treeToValue(jsonNode, ErrorAccessTokenResponse.class);
                         } else {
                             return mapper.treeToValue(jsonNode, SuccessAccessTokenResponse.class);
                         }
@@ -123,14 +158,14 @@ class GithubCli implements Callable<Integer> {
                 .header("Accept", "application/json")
                 .POST(HttpRequest.BodyPublishers.ofString("client_id=" + CLIENT_ID)).build();
 
-        var deviceCodeResponse = client.sendAsync(decivceCodeRequest, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body).thenApply((String s) -> {
+        return client.sendAsync(decivceCodeRequest, HttpResponse.BodyHandlers.ofString()).thenApply(HttpResponse::body)
+                .thenApply((String s) -> {
                     try {
                         return mapper.readValue(s, DeviceCodeResponse.class);
                     } catch (Exception e) {
                         throw new RuntimeException(e);
                     }
                 }).get();
-        return deviceCodeResponse;
+
     }
 }
